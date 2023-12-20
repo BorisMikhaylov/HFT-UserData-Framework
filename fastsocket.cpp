7eb9fa55a6cf103d9b225b32a9b864f9e9f132a4
Boris Mikhaylov <boris.n.mikhaylov@gmail.com>
2023-12-20 22:50:59 +0300

    change delay to 1 microsecond

--- fastsocket.cpp
+++ fastsocket.cpp
@@ -60,7 +60,7 @@ namespace bhft {
                     FD_SET(socket, &rfds);
                     select(socket + 1, &rfds, nullptr, 0, &tv);
                 } else {
-                    std::this_thread::sleep_for(std::chrono::milliseconds(1));
+                    std::this_thread::sleep_for(std::chrono::microseconds(1));
                 }
                 continue;
             } else if (cntReadBytes <= 0) {
